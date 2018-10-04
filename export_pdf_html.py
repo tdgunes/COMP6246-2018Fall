96db4cfbcabcd6adb4b0bcb6def29919d030f13f
Taha Dogan Gunes <tdgunes@gmail.com>
2018-10-04 17:05:31 +0100

    Add html, pdf versions

---
+++ export_pdf_html.py
@@ -0,0 +1,25 @@
+import os
+import glob
+
+def run(command):
+	print(command)
+	os.system(command)
+
+
+notebook_files = [filename for filename in glob.iglob('./lab*/*.ipynb', recursive=True)]
+
+notebook_files.sort(key=lambda x: os.path.basename(os.path.dirname(x)))
+
+pdf_command = "jupyter nbconvert {} --to pdf"
+html_command = "jupyter nbconvert {} --to html --template full"
+
+for f in notebook_files:
+    print(os.getcwd())
+    folder = os.path.dirname(f)
+    print(folder)
+    os.chdir(folder)
+    file_name = os.path.basename(f)
+    run(pdf_command.format(file_name))
+    run(html_command.format(file_name))
+    os.chdir("..")
+    print(os.getcwd())
